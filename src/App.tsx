02b63ab8efc6c5e6eab174dd3d991f52598187ea
Kağan Değirmen <kagandegirmen@gmail.com>
2024-08-15 12:19:56 +0300

    App implemented

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import SudokuBoard from "./components/SudokuBoard/SudokuBoard";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="app-h1">Sudoku Puzzle</h1>
+      <SudokuBoard />
+    </div>
+  );
+}
+
+export default App;
